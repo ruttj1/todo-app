3822535796e5443a307e2dfec04907710651ab24
ruttj1 <76194219+ruttj1@users.noreply.github.com>
2022-03-28 22:49:26 -0400

    display ToDo tasks in the browser

--- todo.js
+++ todo.js
@@ -3,9 +3,19 @@
 var toDo = [ ]; 
 
 function addToDo () {
+	// add users input into array
 	toDo.push(document.getElementById("input").value);  
-	alert("Added " + toDo);  
-	document.getElementById("checkList").innerHTML = toDo; 
+	alert("Added " + toDo);  	
 }  
 // display todo input
- 
+function displayTasks (toDo) {
+	for (var i = 0; i < toDo.length; i++) { 
+		task = toDo[i]; 
+	} 
+		listItem = document.createElement("li"); 
+		displayTask = document.createTextNode(task); 
+		listItem.appendChild(displayTask); 
+		insertAt = document.getElementById("checkList"); 
+		insertAt.appendChild(listItem);  
+}
+

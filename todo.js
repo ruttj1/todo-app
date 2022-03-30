c4a8ec95c2b6f6276ae43b4ac2279a8a1d1da711
ruttj1 <76194219+ruttj1@users.noreply.github.com>
2022-03-29 20:40:59 -0400

    add comments to displayTask function.

--- todo.js
+++ todo.js
@@ -12,10 +12,17 @@ function displayTasks (toDo) {
 	for (var i = 0; i < toDo.length; i++) { 
 		task = toDo[i]; 
 	} 
+		// create element to store task in
 		listItem = document.createElement("li"); 
+		// create contents of list element
 		displayTask = document.createTextNode(task); 
+		// create id for list element
+		listItem.id = task;
+		// insert contents of list element
 		listItem.appendChild(displayTask); 
+		// create location to insert list element 
 		insertAt = document.getElementById("checkList"); 
+		// insert list element to html
 		insertAt.appendChild(listItem);  
 }
 
